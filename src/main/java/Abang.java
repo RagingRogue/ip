bd92aaad3168975b6b332369a270b1e9c5f690c1
Lee Ray Kye <e1337578@u.nus.edu>
2025-08-21 08:49:43 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Abang.java
@@ -0,0 +1,8 @@
+public class Abang {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Abang!");
+        System.out.println("What can I do for you?");
+
+        System.out.println("\nBye. Hope to see you again soon");
+    }
+}
