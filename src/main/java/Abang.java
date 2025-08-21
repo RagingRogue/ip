44d98bc3e2a5e17afc998a8874bf3153092627a8
Lee Ray Kye <e1337578@u.nus.edu>
2025-08-21 09:16:13 +0800

    Echo

--- src/main/java/Abang.java
+++ src/main/java/Abang.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Abang {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Abang!");
         System.out.println("What can I do for you?");
 
-        System.out.println("\nBye. Hope to see you again soon");
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String input = sc.nextLine();
+
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon.");
+                break;
+            }
+
+            System.out.println(input);
+
+        }
     }
 }
