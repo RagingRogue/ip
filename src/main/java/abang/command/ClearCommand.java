f9aa8b33322c2ca84905db08472649a41fa60f89
Lee Ray Kye <e1337578@u.nus.edu>
2025-09-07 16:32:38 +0800

    Add JavaDoc to ClearCommand class

--- src/main/java/abang/command/ClearCommand.java
+++ src/main/java/abang/command/ClearCommand.java
@@ -5,7 +5,12 @@ import abang.ui.UI;
 import abang.storage.Storage;
 import abang.exception.AbangException;
 
-
+/**
+ * Represents a command that clears all tasks from the {@link TaskList}.
+ * <p>
+ * When executed, this command removes all tasks, updates storage,
+ * and provides feedback to the user.
+ */
 public class ClearCommand extends Command{
 
     @Override
